65a04602121434c53115a16861581a469c906c9e
Abhi <abhinandan.srinivas@gmail.com>
2019-12-05 00:38:33 +1100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
+import Actionsheet from './sheet'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			show: false
+		}
+		this.appData = this.appData.bind(this);
+	}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	appData(data){
+
+		this.props.sendData(data+ " "+ this.props.tip+"\n")
+	}
+
+	onClick = () => {
+		const { show } = this.state
+		this.setState({show: !show})
+	}
+   
+	onRequestClose = () => {
+	  this.onClick()
+	}
+	onCancel = () =>{
+
+	}
+	sheetClick = () => {
+		alert('clicked something')
+	}
+	render() {
+	  return (
+		<div>
+		  <button onClick={this.onClick}>{this.props.tip}</button>
+   		  <Actionsheet actionData = {this.appData} show= {this.state.show} menus={this.props.menus} onRequestClose={this.onRequestClose} onClick={this.sheetClick} onCancel={this.onCancel}/>
+		</div>
+	  )
+	}
+  }
+class Disp extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			menus: [{content: 'Little'}, {content: '1'}, {content: '2'}, {content: '3'}, {content: "a lot"}],
+			string: "sms:&body=Hey dan! \nI want\n",
+			
+		}
+		this.getData = this.getData.bind(this);
+	}
+	getData(e){
+		const string = this.state.string;
+		this.setState({string: string+e});
+		alert(string);
+	}
+	
+	render(){
+		return(
+			<div className="menu">
+			<App tip = "Bacon" menus = {this.state.menus} sendData = {this.getData}/>
+			<App tip = "Chicken" menus = {this.state.menus} sendData = {this.getData}/>
+			<App tip = "Beef" menus = {this.state.menus} sendData = {this.getData}/>
+			<App tip = "Parmesan" menus = {this.state.menus} sendData = {this.getData}/>
+			<a href={this.state.string}> All done </a>
+			</div>
+			)
+	  }
+  }
+
+// ========================================
+
+ReactDOM.render(
+  <Disp />,
+  document.getElementById('root')
+);
+
