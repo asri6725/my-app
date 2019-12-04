0c697c3c798db389357bb677cadcb78f13994d0d
Abhi <abhinandan.srinivas@gmail.com>
2019-12-05 01:12:58 +1100

    Updates

---
+++ precache-manifest.6fa17fbaaffa1ac182bfa7f1dfe8ab98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a149187fd888d7a85cd69581720ab3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b8623e3de01add4b6c6a",
+    "url": "/my-app/static/css/main.ec867331.chunk.css"
+  },
+  {
+    "revision": "f7f2f3ac0b5d0852a641",
+    "url": "/my-app/static/js/2.12f500fc.chunk.js"
+  },
+  {
+    "revision": "b8623e3de01add4b6c6a",
+    "url": "/my-app/static/js/main.ec8e9ba6.chunk.js"
+  },
+  {
+    "revision": "2067abcc1b1d9bcd4e07",
+    "url": "/my-app/static/js/runtime-main.98b5c931.js"
+  }
+]);
