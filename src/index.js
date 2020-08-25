f9f67808a2741c7020078baddce79e8d06c3717b
Abhi <abhinandan.srinivas@gmail.com>
2020-08-26 02:00:42 +1000

    Sign In

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import axios from 'axios';
-
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 class Disp extends React.Component{
 
@@ -22,10 +23,65 @@ componentDidMount() {
 	}
   }
 
+  
+  firebase.initializeApp({
+	apiKey: "AIzaSyAeHl1Oi2IjVRVOq66fmNRKOGiq46yg66s",
+	authDomain: "sheetmanipulation.firebaseapp.com"
+  })
+  
+  class App extends React.Component {
+	state = { isSignedIn: false, email: '000' }
+	uiConfig = {
+	  signInFlow: "popup",
+	  signInOptions: [
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+		firebase.auth.GithubAuthProvider.PROVIDER_ID,
+		firebase.auth.EmailAuthProvider.PROVIDER_ID
+	  ],
+	  callbacks: {
+		signInSuccess: () => false
+	  }
+	}
+  
+	componentDidMount = () => {
+	  firebase.auth().onAuthStateChanged(user => {
+		this.setState({ isSignedIn: !!user })
+		if(this.state.isSignedIn == true){
+			this.setState({email: firebase.auth().currentUser.email})
+		}
+		console.log("user", user, "Email", firebase.auth().currentUser.email)
+	  })
+	}
+  
+	render() {
+	  return (
+		<div className="App">
+		  {this.state.isSignedIn ? (
+			<span>
+			  <div>Signed In!</div>
+			  <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+			  <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+			  <img
+				alt="profile picture"
+				src={firebase.auth().currentUser.photoURL}
+			  />
+			</span>
+		  ) : (
+			<StyledFirebaseAuth
+			  uiConfig={this.uiConfig}
+			  firebaseAuth={firebase.auth()}
+			/>
+		  )}
+		</div>
+	  )
+	}
+  }
+  
 // ========================================
 
 ReactDOM.render(
-  <Disp />,
+  <App />,
   document.getElementById('root')
 );
 
