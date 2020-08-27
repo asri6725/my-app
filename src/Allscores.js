23bde9018fe23edf4abb5439ddf1268ad254c5e7
Abhi <abhinandan.srinivas@gmail.com>
2020-08-27 17:47:09 +1000

    Scores view update

--- src/Allscores.js
+++ src/Allscores.js
@@ -2,10 +2,18 @@ import React from 'react';
 import axios from 'axios';
 
 class Allscores extends React.Component{
+  constructor(props){
+		super(props);
+		this.state = {
+      scores : []
+    }
+	}
     componentDidMount() {
-        axios.get('http://18.222.1.20:8080/all-scores/')
+        axios.get('http://localhost:8080/all-scores/')
         .then(response => {
           console.log(response.data);
+          const res = response.data;
+          this.setState({scores: res})
         })
         .catch(error => {
           console.log(error);
@@ -13,7 +21,19 @@ class Allscores extends React.Component{
       }
     render(){
 		return(
-			<p> These are all the scores available. </p>
+			<div>
+      <p> These are all the scores available. </p>
+      <div> <p> Name followed by score. </p></div>
+      <div> 
+        {
+          this.state.scores.map((users, index) => {
+            return(
+              <div>  {users.name} {users.score}  </div>
+            )
+          })
+        }
+        </div>
+      </div>
 		);
 	}
   }
